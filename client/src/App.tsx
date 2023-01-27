e4e76f6d42b66eeb950899b8c044b0a3311ff9a7
win <cosihh55@gmail.com>
2023-01-27 15:59:58 +0900

    feat: 초기 셋팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
